c883536d9198e78e5b0e30f794e44af5a6a90caa
justdoit149 <113504855+justdoit149@users.noreply.github.com>
2023-04-04 20:40:02 +0800

    Update car_problem.py

--- car_problem.py
+++ car_problem.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-"""write your code in method locate_car_problem"""
-
-
 def locate_car_problem():
     if input("Is the car silent when you turn the key?") == "y":
         if input("Are the battery terminals corroded?") == "y":
@@ -21,27 +17,3 @@ def locate_car_problem():
                     else:
                         print("Check to ensure the choke is opening and closing.")
     return
-=======
-"""write your code in method locate_car_problem"""
-
-
-def locate_car_problem():
-    if input("Is the car silent when you turn the key?") == "y":
-        if input("Are the battery terminals corroded?") == "y":
-            print("Clean terminals and try starting again.")
-        else:
-            print("Replace cables and try again.")
-    else:
-        if input("Dose the car make a clicking noise?") == "y":
-            print("Replace the battery.")
-        else:
-            if input("Does the car crank up but fail to start?") == "y":
-                print("Check spark plug connections.")
-            else:
-                if input("Does the engine start and then die?") == "y":
-                    if input("Does your car have fuel injection?") == "y":
-                        print("Get it in for service.")
-                    else:
-                        print("Check to ensure the choke is opening and closing.")
-    return
->>>>>>> 2761c2fec554089fd6ef701c99c5fc6f58c11f62
