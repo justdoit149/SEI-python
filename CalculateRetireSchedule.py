005ffb8796a8a14be873ac72bf1b67edd030e4b1
shanyudong <1427060889@qq.com>
2023-04-13 20:26:55 +0800

    =

--- CalculateRetireSchedule.py
+++ CalculateRetireSchedule.py
@@ -1,8 +1,10 @@
-from datetime import datetime 
+import datetime 
+# from datetime import datetime 第二种写法
 
 CurrentAge = int(input("What is your current age?"))
 RetireAge = int(input("At what age would you like to retire?"))
 years = RetireAge - CurrentAge
-CurrentYear = datetime.now().year   #获取当前时间，并截取出年份
+CurrentYear = datetime.datetime.now().year   #获取当前时间，并截取出年份
+# CurrentYear = datetime.now().year第二种写法
 print("You have {} years left until you can retire.".format(max(years,0)))#注意可能当前年龄大于退休年龄
 print("It's {}, so you can retire in {}.".format(CurrentYear, CurrentYear + years))
